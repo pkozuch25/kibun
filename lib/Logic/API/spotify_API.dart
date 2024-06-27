c60c526327ae33a3e6c1478c6ef2fed8d95d99c0
kosiarn <kosiarn@users.noreply.github.com>
2024-06-27 18:42:07 +0200

    add main Spotify API connector utility

---
+++ lib/Logic/API/spotify_API.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+///Performs a request to Spotify API.
+Future<Map<String, dynamic>> callSpotifyAPI(String endpoint,
+    Map<String, String> headers, {Map<String, String>? body}) async {
+  String? requestParameters = convertBody(body);
+  String endpointURL = requestParameters == null
+      ? "https://api.spotify.com/v1/$endpoint"
+      : "https://api.spotify.com/v1/$endpoint?$requestParameters";
+  http.Response response =
+      await http.get(Uri.parse(endpointURL), headers: headers);
+  return (jsonDecode(response.body));
+}
+
+///Converts body into a GET parameter string.
+String? convertBody(Map<String, String>? requestBody) {
+  List<String> parameters = [];
+  if (requestBody == null) {
+    return null;
+  }
+  requestBody.forEach((name, value) => parameters.add("$name=$value"));
+  String joinedParameters = parameters.join("&");
+  return (joinedParameters);
+}
